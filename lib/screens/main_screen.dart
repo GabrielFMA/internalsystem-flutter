d0c02e6fe9b8a5716124f484209c95535b7c7e24
Gabriel Moreira <gabriel.figueiredo534@gmail.com>
2024-08-08 21:11:34 -0300

    Tela principal com tela de loading configurada

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -4,58 +4,61 @@ import 'package:internalsystem/util/responsive.dart';
 import 'package:internalsystem/widgets/main/screen_widget.dart';
 import 'package:internalsystem/widgets/main/side_menu_widget.dart';
 
-class LoadingScreen extends StatelessWidget {
-  const LoadingScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green[200],
-      body: const Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
-  }
-}
-
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
+  Future<void> _loadData() async {
+    await Future.delayed(const Duration(seconds: 2));
+  }
+
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+      future: _loadData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return buildLoadingScreen();
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(child: Text('Error loading data: ${snapshot.error}')),
+          );
+        } else {
+          return _buildContent(context);
+        }
+      },
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
     final isDesktop = Responsive.isDesktop(context);
 
     return Scaffold(
       drawerScrimColor: const Color.fromARGB(24, 0, 0, 0),
-      drawer: !isDesktop 
-        ? Drawer(
-            child: Container(
-              color: menuColor,
-              child: const SideMenuWidget(),
-            ),
-          )
-        : null,
+      drawer: !isDesktop
+          ? Drawer(
+              child: Container(
+                color: menuColor,
+                child: const SideMenuWidget(),
+              ),
+            )
+          : null,
       body: SafeArea(
         child: Row(
           children: [
             if (isDesktop)
               const Expanded(
                 flex: 2,
-                child: SizedBox(
-                  child: SideMenuWidget(),
-                ),
+                child: SideMenuWidget(),
               ),
             Expanded(
               flex: 10,
               child: Container(
-                child: const ScreenWidget()
+                child: const ScreenWidget(),
               ),
-              
             ),
           ],
         ),
       ),
     );
   }
-  
 }
