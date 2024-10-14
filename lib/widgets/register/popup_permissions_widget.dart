35a5d575f15d59419228037e2dae9eb15f063dc2
Gabriel Moreira <gabriel.figueiredo534@gmail.com>
2024-10-13 21:20:31 -0300

    Permission Pop-Up: Updates to Permission Management
    - Updates to User, Route and Administrator Lists: You can now easily modify permissions by pulling data from a pre-configured list. This simplifies the editing and organization process.
    
    - Implementation of Logic in the Search Bar: The search bar has been improved to be more than just visual. It is now linked to the permissions list, allowing for efficient and accurate searching.
    
    - Saving Selections: Any selected information is now properly saved or sent to the system, ensuring that changes are recorded and accessible.

--- lib/widgets/register/popup_permissions_widget.dart
+++ lib/widgets/register/popup_permissions_widget.dart
@@ -1,58 +1,69 @@
+// ignore_for_file: library_private_types_in_public_api, avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:internalsystem/constants/constants.dart';
 import 'package:internalsystem/utils/responsive.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:internalsystem/data/permissions_data.dart';
 
 class PopUpPermissionsWidget extends StatefulWidget {
+  const PopUpPermissionsWidget({super.key});
+
   @override
   _PopUpPermissionsWidgetState createState() => _PopUpPermissionsWidgetState();
 }
 
 class _PopUpPermissionsWidgetState extends State<PopUpPermissionsWidget> {
   int _selectedButton = 1;
+  String _searchQuery = '';
 
-  final List<String> _permissionsUser = [
-    'Permissão Usuário 1',
-    'Permissão Usuário 2',
-    'Permissão Usuário 3',
-    'Permissão Usuário 4',
-    'Permissão Usuário 5',
-    'Permissão Usuário 6',
-    'Permissão Usuário 7',
-    'Permissão Usuário 8',
-  ];
+  final List<String> _permissionsUser = PermissionsData()
+      .permissionsUsers
+      .map((permission) => permission['title'] as String)
+      .toList();
 
-  final List<String> _permissionsRoutes = [
-    'Permissão de Rota 1',
-    'Permissão de Rota 2',
-    'Permissão de Rota 3',
-    'Permissão de Rota 4',
-    'Permissão de Rota 5',
-    'Permissão de Rota 6',
-    'Permissão de Rota 7',
-    'Permissão de Rota 8',
-  ];
+  final List<String> _permissionsRoutes = PermissionsData()
+      .permissionsRoutes
+      .map((permission) => permission['title'] as String)
+      .toList();
 
-  final List<String> _permissionsAdmin = [
-    'Permissão Admin 1',
-    'Permissão Admin 2',
-    'Permissão Admin 3',
-    'Permissão Admin 4',
-    'Permissão Admin 5',
-    'Permissão Admin 6',
-    'Permissão Admin 7',
-    'Permissão Admin 8',
-  ];
+  final List<String> _permissionsAdmin = PermissionsData()
+      .permissionsAdmin
+      .map((permission) => permission['title'] as String)
+      .toList();
 
-  // Controla o estado dos checkboxes
-  Map<String, bool> _permissionsUserStatus = {};
-  Map<String, bool> _permissionsRoutesStatus = {};
-  Map<String, bool> _permissionsAdminStatus = {};
+  final Map<String, bool> _permissionsUserStatus = {};
+  final Map<String, bool> _permissionsRoutesStatus = {};
+  final Map<String, bool> _permissionsAdminStatus = {};
 
   void _onButtonPressed(int buttonIndex) {
     setState(() {
       _selectedButton = buttonIndex;
     });
+  }
+
+  List<String> _getAllSelectedPermissions() {
+    List<String> selectedPermissions = [];
+
+    _permissionsUserStatus.forEach((permission, isSelected) {
+      if (isSelected) {
+        selectedPermissions.add(permission);
+      }
+    });
+
+    _permissionsRoutesStatus.forEach((permission, isSelected) {
+      if (isSelected) {
+        selectedPermissions.add(permission);
+      }
+    });
+
+    _permissionsAdminStatus.forEach((permission, isSelected) {
+      if (isSelected) {
+        selectedPermissions.add(permission);
+      }
+    });
+
+    return selectedPermissions;
   }
 
   @override
@@ -70,9 +81,9 @@ class _PopUpPermissionsWidgetState extends State<PopUpPermissionsWidget> {
     }
 
     return AlertDialog(
-      title: Center(child: const Text('Selecione as Permissões')),
+      title: const Center(child: Text('Selecione as Permissões')),
       backgroundColor: backgroundColor,
-      content: Container(
+      content: SizedBox(
         width: 500,
         child: Column(
           mainAxisSize: MainAxisSize.min,
@@ -89,51 +100,54 @@ class _PopUpPermissionsWidgetState extends State<PopUpPermissionsWidget> {
                 ],
               ),
             ),
-            SizedBox(height: 15),
-            Container(
-              height: 44,
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Buscar...',
-                  prefixIcon: Padding(
-                    padding: EdgeInsets.only(left: 17.5, right: 15),
-                    child: Icon(
-                      MdiIcons.magnify,
-                      size: 19.5,
-                      color: Colors.white,
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: textFieldColor, width: 1.5),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: textFieldColor, width: 1.5),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Colors.white38, width: 1.5),
-                  ),
-                  hintStyle: TextStyle(color: Colors.grey[600]),
-                  filled: true,
-                  fillColor: Colors.transparent,
-                  contentPadding: const EdgeInsets.symmetric(
-                    vertical: 12,
-                    horizontal: 15,
+            const SizedBox(height: 15),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Buscar...',
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.only(left: 17.5, right: 15),
+                  child: Icon(
+                    MdiIcons.magnify,
+                    size: 19.5,
+                    color: Colors.white,
                   ),
                 ),
-                style: const TextStyle(color: textFieldColor),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: textFieldColor, width: 1.5),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: textFieldColor, width: 1.5),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: Colors.white38, width: 1.5),
+                ),
+                hintStyle: TextStyle(color: Colors.grey[600]),
+                filled: true,
+                fillColor: Colors.transparent,
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 12,
+                  horizontal: 15,
+                ),
+              ),
+              style: const TextStyle(color: textFieldColor),
+              onChanged: (value) {
+                setState(() {
+                  _searchQuery = value;
+                });
+              },
+            ),
+            const SizedBox(height: 15),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Selecione as permissões de "$selectedFilterText"',
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
             ),
-            SizedBox(height: 15),
-            Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Selecione as permissões de "$selectedFilterText"',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                )),
-            SizedBox(height: 15),
+            const SizedBox(height: 15),
             // Display checkboxes based on selected filter
             Expanded(
               child: _buildPermissionsList(selectedFilterText),
@@ -145,14 +159,14 @@ class _PopUpPermissionsWidgetState extends State<PopUpPermissionsWidget> {
         Center(
           child: TextButton(
             onPressed: () {
-              Navigator.of(context).pop();
+              Navigator.of(context).pop(_getAllSelectedPermissions());
             },
             style: TextButton.styleFrom(
               backgroundColor: Colors.white,
-              padding: EdgeInsets.all(16),
-              fixedSize: Size(200, 44),
+              padding: const EdgeInsets.all(16),
+              fixedSize: const Size(200, 44),
               foregroundColor: Colors.white,
-              side: BorderSide(color: Colors.white, width: 1.5),
+              side: const BorderSide(color: Colors.white, width: 1.5),
             ),
             child: const Text(
               'Concluir permissões',
@@ -168,7 +182,6 @@ class _PopUpPermissionsWidgetState extends State<PopUpPermissionsWidget> {
     );
   }
 
-  // Função para construir a lista de checkboxes
   Widget _buildPermissionsList(String selectedFilter) {
     List<String> permissions;
     Map<String, bool> permissionsStatus;
@@ -184,15 +197,19 @@ class _PopUpPermissionsWidgetState extends State<PopUpPermissionsWidget> {
       permissionsStatus = _permissionsAdminStatus;
     }
 
+    final filteredPermissions = permissions.where((permission) {
+      return permission.toLowerCase().contains(_searchQuery.toLowerCase());
+    }).toList();
+
     return ListView.builder(
-      itemCount: permissions.length,
+      itemCount: filteredPermissions.length,
       itemBuilder: (context, index) {
         return CheckboxListTile(
-          title: Text(permissions[index]),
-          value: permissionsStatus[permissions[index]] ?? false,
+          title: Text(filteredPermissions[index]),
+          value: permissionsStatus[filteredPermissions[index]] ?? false,
           onChanged: (value) {
             setState(() {
-              permissionsStatus[permissions[index]] = value!;
+              permissionsStatus[filteredPermissions[index]] = value!;
             });
           },
           activeColor: Colors.white,
@@ -211,10 +228,10 @@ class _PopUpPermissionsWidgetState extends State<PopUpPermissionsWidget> {
       },
       style: TextButton.styleFrom(
         backgroundColor: isSelected ? Colors.white : backgroundColor,
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         fixedSize: Size(isMobile ? 152 : 117, 44),
         foregroundColor: Colors.white,
-        side: BorderSide(color: Colors.white, width: 1.5),
+        side: const BorderSide(color: Colors.white, width: 1.5),
       ),
       child: Center(
         child: Text(
@@ -231,10 +248,29 @@ class _PopUpPermissionsWidgetState extends State<PopUpPermissionsWidget> {
 }
 
 void showDialogPermissions(BuildContext context) {
-  showDialog(
+  showDialog<List<String>>(
     context: context,
     builder: (BuildContext context) {
-      return PopUpPermissionsWidget();
+      return const PopUpPermissionsWidget();
     },
-  );
+  ).then((selectedPermissions) {
+    if (selectedPermissions != null) {
+      final allPermissions = [
+        ...PermissionsData().permissionsUsers,
+        ...PermissionsData().permissionsRoutes,
+        ...PermissionsData().permissionsAdmin,
+      ];
+
+      final permissionMap = {
+        for (var permission in allPermissions)
+          permission['title']: permission['permission'],
+      };
+
+      final matchedPermissions = selectedPermissions.map((selectedTitle) {
+        return permissionMap[selectedTitle];
+      }).where((permission) => permission != null).toList();
+
+      print('Permissões correspondentes: $matchedPermissions');
+    }
+  });
 }
