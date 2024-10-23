bfe6649f96578064a6ad446e6ea94de1ace872fc
Gabriel Moreira <gabriel.figueiredo534@gmail.com>
2024-10-23 17:28:42 -0300

    Alterações na verification utils, authmodel e authstore

--- lib/models/auth_model.dart
+++ lib/models/auth_model.dart
@@ -2,10 +2,12 @@ class AuthModel {
   final String? id;
   final String? name;
   final String? email;
+  final Map? permissions;
 
   AuthModel({
     this.id,
     this.name,
-    this.email, 
+    this.email,
+    this.permissions,
   });
 }
